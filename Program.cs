b491d0959384883976245918c1ab194507fead51
eldarium <eldarium@gmail.com>
2019-11-29 12:17:17 +0300

    оттепель

--- Program.cs
+++ Program.cs
@@ -26,91 +26,30 @@ namespace ZelyaDushitelBot
         static readonly Regex BotTranslateRegex = new Regex(@"^бот, сколько( сейчас)?( будет)? (.+?) (доллар|бакс|гр|евр|бит)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         static readonly Regex BotWeatherRegex = new Regex(@"^(бот, )?(какая )?погода в (.+?)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         static readonly Regex BotWeatherSmallRegex = new Regex(@"^(бот, )?(какая )?погода$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        static readonly YoutubeClient YoutubeClient = new YoutubeClient();
         static readonly string[] Stickers = {"CAADAgADBAAD9SbqFq83NbkmenTRFgQ",
                                              "CAADAgADBQAD9SbqFjlymYiX2Bj7FgQ",
                                              "CAADAgADBgAD9SbqFoVc73WZyzaDFgQ",
                                              "CAADAgADCQAD9SbqFsQl4dm30mvRFgQ"};
         static ITelegramBotClient _client;
-        static readonly ConcurrentBag<string> BannedAuthors = new ConcurrentBag<string>();
-        static readonly ConcurrentBag<long> BannedChannels = new ConcurrentBag<long>();
-        static string _lastAuthor = "";
-        private static int _lastNewAuthorMessageId = -1;
-        private static int _lastNewChannedMessageId = -1;
-        private static long _lastChannelId = -1;
-        private static int _vidosNumber = 0;
-        private static DateTime _runDate = DateTime.Now;
-        private static ConcurrentDictionary<string, string> FailedVideos = new ConcurrentDictionary<string, string>();
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Hello World!");
-            UpdateAuthors();
-            UpdateChannels();
-            GetLastDate();
             Token = File.ReadAllText(AppContext.BaseDirectory + "token").Trim();
             _client = new TelegramBotClient(Token);
             _client.OnMessage += OnMessage;
             _client.OnMessageEdited += OnMessage;// OnMessageEdited;
-            AppDomain.CurrentDomain.UnhandledException += async (object sender, UnhandledExceptionEventArgs args2) =>{
+            AppDomain.CurrentDomain.UnhandledException += async (object sender, UnhandledExceptionEventArgs args2) =>
+            {
                 await _client.SendTextMessageAsync(new ChatId(91740825), $"Unhandled exception!\n{args2.ExceptionObject}\n{(args2.ExceptionObject as Exception).InnerException?.Message}", disableNotification: true);
             };
             _client.StartReceiving(new[] { UpdateType.EditedMessage, UpdateType.Message });
             Console.ReadLine();
-            File.WriteAllText(AppContext.BaseDirectory + "authors.txt", string.Join("\r\n", BannedAuthors.ToArray()));
-            File.WriteAllText(AppContext.BaseDirectory + "channels.txt", string.Join("\r\n", BannedChannels.ToArray()));
-            File.WriteAllText(AppContext.BaseDirectory + "failed_videos.txt", string.Join("\r\n\r\n",
-                FailedVideos.Select(a => $"{a.Key} info:\r\n{a.Value}").ToArray())
-            );
         }
 
         static async void AddOffence(Message message)
         {
-            if (DateTime.Now.Date > _runDate.Date)
-            {
-                _vidosNumber = 0;
-                _runDate = DateTime.Now;
-            }
-            _vidosNumber++;
             await _client.SendStickerAsync(message.Chat.Id, Stickers[new Random((int)DateTime.Now.Ticks).Next(0, 4)], replyToMessageId: message.MessageId);
-            if (_vidosNumber > 1)
-            {
-                if (_vidosNumber == 2)
-                    await File.WriteAllTextAsync(AppContext.BaseDirectory + "date.txt", DateTime.Now.ToString());
-
-            }
-        }
-
-        static async void Remove(Message message) => await _client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
-
-        static void UpdateChannels()
-        {
-            var text = File.ReadAllText(AppContext.BaseDirectory + "channels.txt");
-            foreach (var item in text.Split(Environment.NewLine).ToList())
-            {
-                if (!string.IsNullOrEmpty(item))
-                    BannedChannels.Add(long.Parse(item));
-            }
-
-        }
-
-        static void UpdateAuthors()
-        {
-            var text = File.ReadAllText(AppContext.BaseDirectory + "authors.txt");
-            foreach (var item in text.Split(Environment.NewLine).ToList())
-            {
-                BannedAuthors.Add(item);
-            }
-        }
-
-        static async void GetLastDate()
-        {
-            if (!File.Exists(AppContext.BaseDirectory + "date.txt"))
-                await File.WriteAllTextAsync(AppContext.BaseDirectory + "date.txt", DateTime.Now.ToString());
-            var date = await File.ReadAllTextAsync(AppContext.BaseDirectory + "date.txt");
-            var datetime = DateTime.Parse(date);
-            if (datetime.Date == DateTime.Now.Date)
-                _vidosNumber = 1;
         }
 
         private static HttpStatusCode _lastStatusCode;
@@ -152,8 +91,6 @@ namespace ZelyaDushitelBot
 
         static async void GetExchangeRates(Message m)
         {
-
-            HttpResponseMessage v;
             List<(string, decimal, decimal)> values;
             try
             {
@@ -190,25 +127,14 @@ namespace ZelyaDushitelBot
                 if (message.Type == MessageType.Video)
                 {
                     AddOffence(message);
-                    //Remove(message);
-                }
-                Chat probableChat;
-                if ((probableChat = message.ForwardFromChat) != null)
-                {
-                    if (BannedChannels.Contains(probableChat.Id))
-                    {
-                        AddOffence(message);
-                        Remove(message);
-                    }
-                    _lastChannelId = probableChat.Id;
-                    _lastNewChannedMessageId = message.MessageId;
                 }
             }
             if (message.Type == MessageType.Sticker && message.Sticker != null)
             {
                 Console.WriteLine(message.From.Username + ": [sticker] " + message.Sticker.SetName + $" emoji {message.Sticker.Emoji}" + " file id " + message.Sticker.FileId);
 
-                if (((message.Sticker.SetName?.Contains("Sharij", StringComparison.OrdinalIgnoreCase) ?? false) || (message.Sticker.SetName?.Contains("Shariy", StringComparison.OrdinalIgnoreCase) ?? false)))
+                if (((message.Sticker.SetName?.Contains("Sharij", StringComparison.OrdinalIgnoreCase) ?? false) ||
+                 (message.Sticker.SetName?.Contains("Shariy", StringComparison.OrdinalIgnoreCase) ?? false)))
                 {
                     await _client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
                 }
@@ -217,11 +143,10 @@ namespace ZelyaDushitelBot
             Console.WriteLine(message.From.Username + ": " + message.Text);
             if (message.HasAuthor("alexvojander"))
             {
-                var isBanned = await FindYoutube(message) || message.Entities != null && message.Entities.Any(en => en.Type == MessageEntityType.Url);
+                var isBanned = message.Entities != null && message.Entities.Any(en => en.Type == MessageEntityType.Url);
                 if (isBanned)
                 {
                     AddOffence(message);
-                    //Remove(message);
                     return;
                 }
             }
@@ -270,95 +195,22 @@ namespace ZelyaDushitelBot
                     await _client.SendTextMessageAsync(message.Chat.Id, "не балуйся");
                 }
             }
-            if(message.HasRegexIgnoreMention(BotWeatherRegex)){
+            if (message.HasRegexIgnoreMention(BotWeatherRegex))
+            {
                 var we = new WeatherWorker();
                 var m = BotWeatherRegex.Match(message.Text).Groups.Last();
-                if(m.Value.Contains("киев", StringComparison.InvariantCultureIgnoreCase)){
-                   await _client.SendTextMessageAsync(message.Chat.Id, we.GetWeather("kyiv"));
+                if (m.Value.Contains("киев", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await _client.SendTextMessageAsync(message.Chat.Id, we.GetWeather("kyiv"));
                 }
-                if(m.Value.Contains("днепр", StringComparison.InvariantCultureIgnoreCase)){
+                if (m.Value.Contains("днепр", StringComparison.InvariantCultureIgnoreCase))
+                {
                     await _client.SendTextMessageAsync(message.Chat.Id, we.GetWeather("dnipro"));
                 }
             }
             if (message.HasCommand("/command4"))
                 await _client.SendTextMessageAsync(message.Chat.Id,
                     $"эта команда подкидывает маму зелика - результат {new Random().Next(0, 2) == 1}");
-
-            if (message.HasAuthor("daneldarium"))
-            {
-                if (message.HasCommand("/command1"))
-                {
-                    // add last video's author to banned authors
-                    if (!string.IsNullOrEmpty(_lastAuthor))
-                    {
-                        BannedAuthors.Add(_lastAuthor);
-                        await _client.DeleteMessageAsync(message.Chat.Id, _lastNewAuthorMessageId);
-                    }
-                }
-                else if (message.HasCommand("/command2"))
-                {
-                    // add last channel to banned channels
-                    if (_lastChannelId != -1)
-                    {
-                        BannedChannels.Add(_lastChannelId);
-                        await _client.DeleteMessageAsync(message.Chat.Id, _lastNewChannedMessageId);
-                    }
-                }
-                else if (message.HasCommand("/command3"))
-                {
-                    _vidosNumber++;
-                    await _client.SendTextMessageAsync(message.Chat.Id,
-                        "@alexvojander первая за сегодня добавлена рофланЕбало");
-                }
-            }
-        }
-
-        static async Task<bool> FindYoutube(Message message)
-        {
-            if (YoutubeRegex.IsMatch(message.Text))
-            {
-                var url = YoutubeRegex.Matches(message.Text)[0].Value.Trim('/');
-                var uri = new Uri("https://" + url);
-                var id = "";
-                var q = HttpUtility.ParseQueryString(uri.Query);
-                if (q.AllKeys.Contains("v"))
-                {
-                    id = q["v"];
-                }
-                else
-                {
-                    id = uri.Segments.Last();
-                }
-                var videoInfo = new YoutubeExplode.Models.Video(
-                    "", "", new DateTimeOffset(), "", "", new YoutubeExplode.Models.ThumbnailSet(""),
-                    TimeSpan.FromSeconds(0), new List<string>(), new YoutubeExplode.Models.Statistics(0, 0, 0));
-                try
-                {
-                    videoInfo = await YoutubeClient.GetVideoAsync(id);
-                    Console.WriteLine("video success!");
-                }
-                catch (Exception e)
-                {
-                    var ex = e as HttpRequestException;
-                    //IDGAF await _client.SendTextMessageAsync(new ChatId(91740825), $"Video {id} failed : {e}", disableNotification: true);
-                    Console.WriteLine($"Video {id} failed : {e}");
-                    FailedVideos.GetOrAdd(id, $"message from {message.From} id {message.MessageId}" +
-                                              $"at {DateTime.Now}\r\n" +
-                                              $"exception {e}\r\n" +
-                                              $"{e.StackTrace}");
-                    return true;
-                }
-                var author = videoInfo.Author;
-                if (!string.IsNullOrEmpty(author))
-                {
-                    _lastAuthor = author;
-                    _lastNewAuthorMessageId = message.MessageId;
-                    var isBanned = BannedAuthors.Contains(author);
-                    Console.WriteLine($"{author} @ {DateTime.Now} - {isBanned}");
-                    return isBanned;
-                }
-            }
-            return false;
         }
     }
 }
